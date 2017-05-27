09f73008f59ea0260d858e887be713bcc762da40
Luke Fries <luke@ninyo.com>
2017-05-27 13:15:23 +1000

    views

--- application.py
+++ application.py
@@ -3,7 +3,9 @@ I am a flask app
 """
 
 from flask import Flask
+from flask import request
 from flask import render_template
+from utils import generate_unravelled_text
 app = Flask(__name__)
 app.config.from_object('siteconfig')
 
@@ -11,6 +13,17 @@ app.config.from_object('siteconfig')
 @app.route("/")
 def hello():
     return render_template('index.html'), 200
+
+@app.route('/', methods=['POST'])
+def query():
+
+    text = request.form['text']
+    processed_text = generate_unravelled_text(text)
+    return render_template('query_result.html', disp_text=processed_text, topic_name=text), 200
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html'), 200
 
 @app.errorhandler(404)
 def not_found(error):
@@ -24,5 +37,9 @@ def server_issues(error):
 def server_maintenance():
     return render_template('maintenance.html'), 200
 
+@app.route('/favicon.ico')
+def favicon():
+    return "", 200
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,12 @@
+"""
+general utils for unravel project
+- web get
+- text parse
+- hyperlink traverse
+- word similarity
+- text reconstruct
+- job metadata
+"""
+
+def generate_unravelled_text(input_text):
+	return input_text
